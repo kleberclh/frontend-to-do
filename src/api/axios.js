64e93d5cdd833849182c31071d477ab3a212e930
Kleber Coelho <klebercoelhogt@gmail.com>
2025-01-12 15:15:42 -0300

    ajustado url da api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/";
+const API_URL = "https://backend-to-do-a8hp.onrender.com/";
 
 const api = axios.create({
   baseURL: API_URL,
